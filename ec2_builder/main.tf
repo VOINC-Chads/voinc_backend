cd300d26139929177966f983ca27acba18408a55
nathanhunsberger <nathan.t.hunsberger@vanderbilt.edu>
2023-04-04 15:30:08 -0500

    Simple test commit to see how workflow runs

--- ec2_builder/main.tf
+++ ec2_builder/main.tf
@@ -1,15 +1,3 @@
-# Command I expect backend to run from inside the ec2_builder directory
-# terraform apply -var 'images=[{"instance_id": "value1", "image": "value2"}, {"instance_id": "value3", "image": "value4"}]'
-
-# PUSH TO ECR
-# sudo aws ecr get-login-password --region us-east-2 | sudo docker login --username AWS --password-stdin 997625559881.dkr.ecr.us-east-2.amazonaws.com
-# sudo docker build -t voinc_repo .
-# sudo docker tag voinc_repo:<INSTANCE_UUID> 997625559881.dkr.ecr.us-east-2.amazonaws.com/voinc_repo:<INSTANCE_UUID>
-# sudo docker push 997625559881.dkr.ecr.us-east-2.amazonaws.com/voinc_repo:<INSTANCE_UUID>
-
-# PULL FROM ECR
-# sudo docker pull 997625559881.dkr.ecr.us-east-2.amazonaws.com/voinc_repo:<INSTANCE_UUID>
-
 locals {
   my_list_of_dicts = jsondecode(var.images)
 }
@@ -37,18 +25,18 @@ resource "aws_instance" "my_instances" {
 
 
          # Wait for the Docker container to finish running the program
-         while docker ps --format '{{.Names}}' | grep -q 'container-${each.key}'; do
-             sleep 1
-         done
+         # while docker ps --format '{{.Names}}' | grep -q 'container-${each.key}'; do
+         #    sleep 1
+         #done
 
          # Send the final result back to a client using an HTTP response
-         INSTANCE_ID=\$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
-         FINAL_RESULT=\$(docker logs container-${each.key} | tail -1)
+         #INSTANCE_ID=\$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
+         #FINAL_RESULT=\$(docker logs container-${each.key} | tail -1)
          # NEED WHERE TO SEND BACK
-         curl -X POST -d "{\\"result\\": \\"\$FINAL_RESULT\\"}" http://client-server.com/result --header "Content-Type:application/json"
+         #curl -X POST -d "{\\"result\\": \\"\$FINAL_RESULT\\"}" http://client-server.com/result --header "Content-Type:application/json"
 
          # Terminate the EC2 instance
-         aws ec2 terminate-instances --instance-ids \$INSTANCE_ID --region ${var.region}
+         #aws ec2 terminate-instances --instance-ids \$INSTANCE_ID --region ${var.region}
          EOF
 
   tags = {
