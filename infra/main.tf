179cda691a71981cb6c831d7d421272d9578750a
nathanhunsberger <nathan.t.hunsberger@vanderbilt.edu>
2023-04-11 19:13:42 -0500

    Allow ssh in the masters and workers so we can debug:)

--- infra/main.tf
+++ infra/main.tf
@@ -64,6 +64,14 @@ resource "aws_security_group" "sgs" {
   vpc_id      = aws_vpc.vpcs[count.index].id
 
   ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     description = "HTTP"
     from_port   = 80
     to_port     = 80
