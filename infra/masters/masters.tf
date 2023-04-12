d247ec3b15a2b749e56a48df2a1d714be6eda187
nathanhunsberger <nathan.t.hunsberger@vanderbilt.edu>
2023-04-11 19:33:39 -0500

    start docker

--- infra/masters/masters.tf
+++ infra/masters/masters.tf
@@ -14,7 +14,8 @@ resource "aws_instance" "ec2_master" {
   user_data = <<EOF
          #!/bin/bash
          sudo yum update -y
-         sudo install docker
+         sudo yum install docker
+         sudo systemctl start docker
          sudo aws ecr get-login-password --region us-east-2 | sudo docker login --username AWS --password-stdin 997625559881.dkr.ecr.us-east-2.amazonaws.com
          sudo docker pull 997625559881.dkr.ecr.us-east-2.amazonaws.com/voinc_repo:${var.image}
          docker run -p 8000:8000 ${var.image}
--- infra/workers/workers.tf
+++ infra/workers/workers.tf
@@ -13,6 +13,9 @@ resource "aws_instance" "ec2_worker" {
   
   user_data = <<EOF
          #!/bin/bash
+         sudo yum update -y
+         sudo yum install docker
+         sudo systemctl start docker
          sudo aws ecr get-login-password --region us-east-2 | sudo docker login --username AWS --password-stdin 997625559881.dkr.ecr.us-east-2.amazonaws.com
          sudo docker pull 997625559881.dkr.ecr.us-east-2.amazonaws.com/voinc_repo:${var.image}
          docker run -p 8000:8000 ${var.image} --ip ${var.master_ip}
